241810c0763a1a762fe3919de6a6414440fb70a4
Christina <chriskreza@csd.auth.gr>
2021-11-22 15:42:52 +0100

    Added info if dest not reached

--- hop_analyzer.py
+++ hop_analyzer.py
@@ -4,6 +4,7 @@ import plotly.graph_objects as go
 import socket
 import sys
 from traceroute import Traceroute
+import requests
 
 def initialize_dataframe(hops):
     df = pd.DataFrame(columns=['Hop', 'Latitude', 'Longitude', 'Country', 'ISP', 'RTT'])
@@ -15,6 +16,7 @@ def initialize_dataframe(hops):
                             'Longitude': hop["longitude"],
                             'Country': hop["country"],
                             'ISP': hop["isp"],
+                            'AS': hop["as"].split()[0],
                             'RTT': hop["rtt"]},
                             ignore_index=True)
         counter = hop["hop_num"]
@@ -48,13 +50,24 @@ def generate_map(df):
             'zoom': 1})
     fig.show()
 
+if __name__ == '__main__':
+    dest_ip = sys.argv[1] if ipv4.validate_ip(sys.argv[1]) else socket.gethostbyname(sys.argv[1])
+    tracerouteLO = Traceroute(dest_ip, country="LO")
+    hopsLO = tracerouteLO.traceroute()
+    # print_formatted_output(hopsLO)
 
-ip = sys.argv[1] if ipv4.validate_ip(sys.argv[1]) else socket.gethostbyname(sys.argv[1])
-tracerouteLO = Traceroute(ip, country="LO")
-hopsLO = tracerouteLO.traceroute()
-# print_formatted_output(hopsLO)
+    dfLO = initialize_dataframe(hopsLO)
+    print(dfLO)
 
-dfLO = initialize_dataframe(hopsLO)
-print(dfLO)
+    last_ip = hopsLO[-1]["ip_address"]
+    if last_ip != dest_ip:
+        print()
+        original_country = requests.get("http://ip-api.com/json/{}".format(dest_ip))
+        print("Desired destination ip: " + dest_ip)
+        print("Desired destination country: " + original_country.json()["country"])
 
-generate_map(dfLO)
+        reached_country = requests.get("http://ip-api.com/json/{}".format(last_ip))
+        print("Destination not reached")
+        print("Traceroute ended to: " + reached_country.json()["country"])
+
+    generate_map(dfLO)
