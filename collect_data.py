a35d514b1a8efdb77d0144474216e2128ee0522a
Christina <chriskreza@csd.auth.gr>
2021-11-22 15:43:12 +0100

    Added script to write data in file

---
+++ collect_data.py
@@ -0,0 +1,45 @@
+from datetime import date, datetime
+from numpy import inf
+from traceroute import Traceroute
+import json
+import requests
+
+def read_file(country):
+    data = []
+    with open("example_ips.txt") as file_object:
+        for line in file_object:
+            if line != "\n":
+                ip = line.rstrip()
+                traceroute = Traceroute(ip, country=country)
+                hops = traceroute.traceroute()
+                info_json = collect_info(ip, hops)
+                data.append(info_json)
+    write_file(data, country)
+
+def collect_info(ip, hops):
+    dest_info = requests.get("http://ip-api.com/json/{}".format(ip))
+    info = {
+        "dest_ip": ip,
+        "dest_country": dest_info.json()["country"],
+        "total_hops": hops[-1]["hop_num"],
+        "countries": [],
+        "rtt": [],
+        "as": [],
+        "isp": []
+    }
+                
+    for hop in hops:
+        if len(info["countries"]) == 0 or info["countries"][-1] != hop["country"]:
+            info["countries"].append(hop["country"])
+        info["rtt"].append(hop["rtt"])
+        info["as"].append(hop["as"])
+        info["isp"].append(hop["isp"])
+
+    return info
+
+def write_file(data, country):
+    pathfile = "data/" + country + "-" + str(date.today().strftime("%d-%m-%Y")) + "-" + str(datetime.now().strftime("%H-%M")) + ".json"
+    with open(pathfile, 'w') as f:
+        json.dump(data, f)
+
+read_file("LO")
